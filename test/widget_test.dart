d2164851fb9eee53a171fc0181cd945f2298dc8d
Haeseong Jeon <seong0428@gmail.com>
2023-01-16 21:29:57 +0900

    config: 카카오 로그인 sdk 추가

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:three_days_flutter/main.dart';
+import 'package:three_days/three_days_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ThreeDaysApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
